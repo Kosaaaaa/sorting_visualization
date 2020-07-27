f2309697c479fd04cab9fb68cec868ab7d13ed8d
Kosaaaaa <oskarkosobucki@gmail.com>
2020-07-27 22:34:13 +0200

    add Settings class

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    # Canvas settings
+    WIDTH = 1000
+    HEIGHT = 600
+    ROWS = 40
+
+    # Colors
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    PINK = (255, 0, 153)
+    GRAY = (192, 192, 192)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
